dfca58036df97e5fad511a28180e101b00281c61
Narmadaw <narmadaw9@gmail.com>
2023-10-22 11:03:41 -0700

    add styles for video list

--- src/components/SelectedVideo/SelectedVideo.jsx
+++ src/components/SelectedVideo/SelectedVideo.jsx
@@ -3,10 +3,23 @@ import "./SelectedVideo.scss";
 
 function SelectedVideo(props){
     return(
-        <section className="selected-video">
+        <section className="video-player">
             {/* <h2>{props.selectedVideo.title}</h2> */}
-            <img className="selected-video__video" src={props.selectedVideo.image} alt="" />
+            <div className="video-player__background">
+                <img className="video-player__video" src={props.selectedVideo.image} alt="" />
+            </div>
+            
+            <div className="video-container">
+                <div className="video-container__title">{props.selectedVideo.title}</div>
+                <div className="video-container__title">{props.selectedVideo.channel}</div>
+                <div className="video-container__title">{props.selectedVideo.timestamp}</div>
+                <div className="video-container__title">{props.selectedVideo.views}</div>
+                <div className="video-container__title">{props.selectedVideo.likes}</div>
+                <div className="video-container__title">{props.selectedVideo.description}</div>
+            </div>
         </section>
+
+        
 
     );
 
--- src/components/VideoList/VideoList.jsx
+++ src/components/VideoList/VideoList.jsx
@@ -2,29 +2,33 @@ import './VideoList.scss';
 
 function VideoList(props){
     return(
-        <ul className="video-list">
-            {props.videoData
-            .filter((video) => video.id !== props.selectedVideo.id)
-            .map((video) => {
-                return(
+        <section className="l-video-list">
+            <h1 className="section-title">NEXT VIDEOS</h1>
+            <ul className="video-list">
+                {props.videoData
+                .filter((video) => video.id !== props.selectedVideo.id)
+                .map((video) => {
+                    return(
                     <li
                     onClick={() => {
                         props.handleVideoSelection(video.id);
                     }}
                     key={video.id}
-                    className="video-list"
-                    >
-                        <h2>{video.title}</h2>
-                        <img src={video.image} alt="" />
+                    className="video-list__item">
+                        <div className="video-list__video-card">
+                            <img className="video-list__video" src={video.image} alt="video" />
+                        </div>
+                        <div className="video-list__text-container">
+                            <p className="video-list__title">{video.title}</p>
+                            <p className="video-list__channel">{video.channel}</p>
+                        </div>
                     </li>
-
-                );
-            })
-            
-            }
-        </ul>
+                    );
+                })
+                }
+            </ul>
+        </section>
     );
-
 }
 
 export default VideoList;
