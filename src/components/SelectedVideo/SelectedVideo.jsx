2c9ff702e58b8141050215308f4debc2d210884f
Narmadaw <narmadaw9@gmail.com>
2023-10-22 03:24:13 -0700

    add media queris to main video section

--- src/components/SelectedVideo/SelectedVideo.jsx
+++ src/components/SelectedVideo/SelectedVideo.jsx
@@ -1,8 +1,11 @@
+import "./SelectedVideo.scss";
+
+
 function SelectedVideo(props){
     return(
         <section className="selected-video">
-            <h2>{props.selectedVideo.title}</h2>
-            <img src={props.selectedVideo.image} alt="" />
+            {/* <h2>{props.selectedVideo.title}</h2> */}
+            <img className="selected-video__video" src={props.selectedVideo.image} alt="" />
         </section>
 
     );
