e47dc5627982f32401b827ad213a620039c1f493
Narmadaw <narmadaw9@gmail.com>
2023-10-29 12:52:10 -0700

    upload page styles done

--- src/pages/VideoUploadPage/VideoUploadPage.jsx
+++ src/pages/VideoUploadPage/VideoUploadPage.jsx
@@ -20,38 +20,31 @@ function VideoUploadPage(){
     return (
         <>
         <section className="upload">
-                <h1 className="upload__title">Upload Video</h1>
-                
-                    <form onSubmit={handleFormSubmit}>
-                    <div className="upload__container">
-                        <div className="upload__left">
-                            <label className='upload__label'>
-                                VIDEO THUMBNAIL
-                                <img className="upload__thumbnail" src={uploadImage} alt="a blue bike"/>
-                            </label>    
-                        </div>
+            <h1 className="upload__title">Upload Video</h1>
+            <div className="upload__container">
+                <form onSubmit={handleFormSubmit}>
+                    <div className='upload__wrapper'>
+                    <div className="upload__left-image-panel">
+                            <h4 className='upload__label'>VIDEO THUMBNAIL</h4> 
+                                <img className="upload__thumbnail" src={uploadImage} alt="a blue bike"/> 
+                    </div>
+                    <div className="upload__right-input-panel">
+                            <h4 className='upload__label'>TITLE YOUR VIDEO</h4>
+                            <input className="upload__input" name="title" placeholder="Add a title to your video"/>
+                            
+                            <h4 className='upload__label upload__label--textarea'>ADD A VIDEO DESCRIPTION</h4>
+                            <textarea className="upload__textarea" rows="7" name="text" placeholder="Add a description to your video"></textarea>
+                    </div>
 
-                        <div className="upload__right">
-                            <label className='upload__label'>
-                                TITLE YOUR VIDEO
-                                <input className="upload__input" name="title" placeholder="Add a title to your video"/>
-                            </label>
-
-                            <label className='upload__label'>
-                                ADD A VIDEO DESCRIPTION
-                                <textarea className="upload__textarea" rows="7" name="text" placeholder="Add a description to your video"></textarea>
-                            </label>
-                        </div>
-                        </div>
-                        <div className="upload__action">
+                    </div>
+                   
+                    <div className="upload__button-panel">
                             <button className="upload__submit">PUBLISH</button>
                             <Link to="/"><button className="upload__link">CANCEL</button></Link>
-                        </div>
-                        
-                    </form>
-                
-                    
-            </section>
+                    </div>
+                </form>
+            </div>       
+        </section>
         </>
     );
 
