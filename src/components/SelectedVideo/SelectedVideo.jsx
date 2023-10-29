1405a2b530ee211391e7d479fe16ae0725e6aa24
Narmadaw <narmadaw9@gmail.com>
2023-10-29 13:05:58 -0700

    fixed some styles in default video section

--- src/components/SelectedVideo/SelectedVideo.jsx
+++ src/components/SelectedVideo/SelectedVideo.jsx
@@ -5,12 +5,13 @@ function SelectedVideo({image, video}){
     
     
     return(
-        
-        <section className="video-player">
-                <video className="background-video" poster={image} controls>
+        <div className="video-player">
+            <div className="wrapper">
+            <video className="background-video" poster={image} controls>
                 <source src= {`${video}?api_key=test`} type="video/mp4"/>
-            </video> 
-        </section>
+            </video>
+            </div>  
+        </div>
     );
 }
 
