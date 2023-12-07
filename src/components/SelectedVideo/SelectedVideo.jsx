7fa68fb8e6ba50f76b7771614cb894de971e0a6e
Narmadaw <narmadaw9@gmail.com>
2023-12-07 10:12:44 -0800

    dd

--- src/components/SelectedVideo/SelectedVideo.jsx
+++ src/components/SelectedVideo/SelectedVideo.jsx
@@ -5,7 +5,7 @@ const SelectedVideo = ({image, video}) =>{
     return(
         <div className="video-player">
             <div className="video-player__wrapper">
-            <video className="video-player__background-image" poster={`${process.env.REACT_APP_API_URL}/${video.image}`} controls>
+            <video className="video-player__background-image" poster={`${process.env.REACT_APP_API_URL}/${image}`} controls>
                 {/* <source className="video-player__background-video" src={video} type="video/mp4"/> */}
                 <source className="video-player__background-video" src="" type="video/mp4"/>
             </video>
